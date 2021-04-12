52461cc0e18a5a36fe7bd1a6574c183878b6305e
Moonshine <710159940@qq.com>
2021-04-12 14:12:25 +0800

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <div class="box">
+      <span>123</span>
+    </div>
+    <div class="box2">
+
+    </div>
+    <div class="box3">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.home {
+  .box{
+    width: 200px;
+    height: 200px;
+    background: #42b983;
+    font-size: 22px;
+  }
+  .box2 {
+    width: 200px;
+    height: 200px;
+    margin-left: 200px;
+    background: salmon;
+  }
+  .box3 {
+    width: 200px;
+    height: 200px;
+    margin-left: 400px;
+    background: sandybrown;
+  }
+}
+</style>
